6fa5f98ae0f3897b1e82804ad6aa2ee79f32c8ac
arturoyj <jayj22022email.com@gmail.com>
2025-10-23 01:39:05 -0600

    configurando componentes de home

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   spotify: {
-    clientId: 'YOUR_SPOTIFY_CLIENT_ID',
-    clientSecret: 'YOUR_SPOTIFY_CLIENT_SECRET',
+    clientId: '7c2a4de796254c29adaa44207f806787',
+    clientSecret: d06c76e5960341639788b7ce2b9a3615',
     authUrl: 'https://accounts.spotify.com/api/token',
     apiUrl: 'https://api.spotify.com/v1'
   }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   spotify: {
-    clientId: 'TU_CLIENT_ID_AQUI',
-    clientSecret: 'TU_CLIENT_SECRET_AQUI',
+    clientId: '7c2a4de796254c29adaa44207f806787',
+    clientSecret: 'd06c76e5960341639788b7ce2b9a3615',
     authUrl: 'https://accounts.spotify.com/api/token',
     apiUrl: 'https://api.spotify.com/v1'
   }
