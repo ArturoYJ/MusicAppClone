3705c176587db820cad7f24e6e28517d040c5463
arturoyj <jayj22022email.com@gmail.com>
2025-10-23 23:21:19 -0600

    quité /... :)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,9 @@ export const environment = {
   production: false,
   spotify: {
     clientId: '7c2a4de796254c29adaa44207f806787',
-    clientSecret: '97b9abb5a94fa19f432627e073af452',
+    clientSecret: '97b9abb5a5e94fa19432627e073af452',
     
     authUrl: 'https://accounts.spotify.com/api/token',
-    apiUrl: 'https://api.spotify.com/v1/...'
+    apiUrl: 'https://api.spotify.com/v1'
   }
 };
