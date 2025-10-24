0991d7a91c46dde4b00a6aad823760cccd180925
arturoyj <jayj22022email.com@gmail.com>
2025-10-23 21:54:10 -0600

    retocando

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
   spotify: {
-    clientId: '7c2a4de796254c29adaa44207f806787',
-    clientSecret: 'd06c76e5960341639788b7ce2b9a3615',
+    clientId: '6ac92f22cea14a98b59da670ff894528',
+    clientSecret: '50ace17b954d48ceab22651c6f556cb2',
     authUrl: 'https://accounts.spotify.com/api/token',
-    apiUrl: 'https://api.spotify.com/v1'
+    apiUrl: 'https://api.spotify.com/v1/...'
   }
 };
